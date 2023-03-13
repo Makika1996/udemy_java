18e34b98a00ac41e33f09c26a9655645575ad690
Marija Marinkov <Marija Marinkov@eiskonzept.ag>
2023-03-13 17:25:47 +0100

    InputStreamReaderOutputStreamWriter

---
+++ src/io/InputStreamReaderOutputStreamWriter.java
@@ -0,0 +1,41 @@
+package io;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import static java.nio.charset.StandardCharsets.*;
+
+/**
+ * Lernziel: Umwandlung `Reader`/`Writer` in `InputStream`/`OutputStream`
+ * - Kodieren (Unicode-Zeichen → Bytefolge), Decodieren (Bytefolge → Unicode-Zeichen)
+ * - `InputStreamReader`
+ * - `OutputStreamWriter`
+ * - `StandardCharsets`
+ */
+
+public class InputStreamReaderOutputStreamWriter {
+    public static void main(String[] args) {
+        // InputStream -> InputStream -> InputStream -> R. InputStream
+        // OutputStream -> OutputStream -> OutputStream -> R. OutputStream
+        // Writer -> Writer -> Writer -> Writer -> R. Writer
+        // Reader -> Reader -> Reader -> Reader -> R. Reader
+
+        try (OutputStream out = Files.newOutputStream(Paths.get("text.txt"));
+             Writer osw = new OutputStreamWriter(out, UTF_8);) {
+            osw.write("Hallo →");
+            osw.write(" Welt!");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try (InputStream is = Files.newInputStream(Paths.get("text.txt"));
+             Reader osw = new InputStreamReader(is, UTF_8);
+             BufferedReader br = new BufferedReader(osw)) {
+            System.out.println(br.readLine());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
