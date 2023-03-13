0bbf38c197db05ad7cd34be491b7735f3f826c36
Marija Marinkov <Marija Marinkov@eiskonzept.ag>
2023-03-13 12:15:05 +0100

    InputStreamOutputStreamReaderWriter

--- src/io/InputStreamOutputStreamReaderWriter.java
+++ src/io/InputStreamOutputStreamReaderWriter.java
@@ -1,5 +1,10 @@
 package io;
 
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.time.LocalDateTime;
+
 /**
  * Lernziel: Ein-/Ausgabeströme
  * `InputStream`
@@ -10,6 +15,23 @@ package io;
  */
 
 public class InputStreamOutputStreamReaderWriter {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+//        StringWriter writer = new StringWriter();
+//        writeNow(writer);
+//        System.out.println(writer.toString());
+        try (Writer writer = Files.newBufferedWriter(Paths.get("datum-zeit.txt"))) {
+            writeNow(writer);
+        }
+        try (Reader reader = Files.newBufferedReader(Paths.get("datum-zeit.txt"))) {
+//            while (reader.ready()) {
+//                System.out.println((char) reader.read());
+//            }
+            for (int c; (c = reader.read()) != -1; )
+                System.out.println((char) c);
+        }
+    }
+
+    private static void writeNow(Writer writer) throws IOException {
+        writer.write(LocalDateTime.now().toString());
     }
 }
