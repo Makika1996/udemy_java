2586ddecc26948c2f6d57aa01ae1f6cfeb2e4ddb
Marija Marinkov <Marija Marinkov@eiskonzept.ag>
2023-03-13 17:54:10 +0100

    Comments

---
+++ src/Application.java
@@ -0,0 +1,16 @@
+/**
+ * Lernziel:
+ * - Klassen > Methoden > Code
+ * - main(...)
+ * - Bezeichner
+ * - Groß-/Kleinschreibung
+ * - Reservierte Schlüsselwörter
+ * - Modifizierer
+ * - Einrückung
+ */
+
+class Application {
+    public static void main(String[] args) {
+        System.out.println("Hallo Welt!");
+    }
+}
---
+++ src/Comments.java
@@ -0,0 +1,18 @@
+/**
+ * Lernziel:
+ * - Zeilenkommentar
+ * - Blockkommentar
+ * - Javadoc
+ */
+
+public class Comments {
+    // Zeilenkommentar //
+
+    /**
+     * Hauptprogramm.
+     *
+     * @param args Übergabe an die JVM.
+     */
+    public static void main(String[] args) {
+    }
+}
